220f5a040201081cac29d4bb78fac83037e60194
Henrique Nogueira <henrique.nogdsantos@gmail.com>
2025-04-14 20:37:14 -0300

    Criação da main e outras funções

--- Formação Aprenda a programar em Python com Orientação a Objetos/Sabor Express - alura/app.py
+++ Formação Aprenda a programar em Python com Orientação a Objetos/Sabor Express - alura/app.py
@@ -1,6 +1,7 @@
 import os
 
-print("""
+def exibir_nome_do_programa():
+      print("""
       
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -10,25 +11,34 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-
 def finalizar_app():
     os.system('cls' if os.name == 'nt' else 'clear')
     print('Finalizando o app\n')
 
+def exibir_opcoes():
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurante')
+      print('3. Ativar restaurante')
+      print('4. Sair\n')
 
-if opcao_escolhida == 1: 
-    print('Cadastrar restaurante')
+def escolher_opcao():   
+      opcao_escolhida = int(input('Escolha uma opção: '))
+      
+      if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
 
-elif opcao_escolhida == 2:
-    print('Listar restaurante')
+      elif opcao_escolhida == 2:
+            print('Listar restaurante')
 
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+      elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+      else:
+            finalizar_app()
+
+def main():
+      exibir_nome_do_programa()
+      exibir_opcoes()
+      escolher_opcao()
+
+if __name__ == '__main__': 
+      main()
